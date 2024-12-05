5d049f6a0d421d15567314e1f52d1a4288d6e300
Florian18 <florian.mancieri@gmail.com>
2024-12-05 08:48:51 +0100

    publication exercice
    Exercice AJAX

---
+++ assets/js/ajax.js
@@ -0,0 +1,3 @@
+// A l'aide de l'api de géocalisation https://adresse.data.gouv.fr/api-doc/adresse
+// Retourner l'ensemble des villes en fonction du code postal
+// Faire 3 requêtes AJAX (xhr, fetch, jquery)
